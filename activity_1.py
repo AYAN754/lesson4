0d1dcc64ee9b5e4d4076f2f728f9c28983fd7db8
AYAN754 <ayanalizai960@gmail.com>
2024-10-28 22:02:33 +0500

    myfirst comitt

---
+++ activity_1.py
@@ -0,0 +1,22 @@
+a = 52
+b = 23
+c = 45
+if (a>b and b<c):
+    print("a and c are greater than b")
+else:
+    print("these are not greater then b")
+
+r = 52
+g = 23
+y = 45
+if (r>g or y<g):
+    print("g and r are greater than y")
+else:
+    print("these are not greater then g")
+
+s = 52
+
+if (not s):
+    print("true")
+else:
+    print("false")
---
+++ activity_2.py
@@ -0,0 +1,7 @@
+a = 10
+b = 18
+c = 10
+print(a != b)
+print(a == c)
+print(a == b)
+print(a != c)
